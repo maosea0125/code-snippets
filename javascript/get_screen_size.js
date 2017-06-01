6c28f142b9079ff362eed3964950ece67856a942
maofei <maofei@rsung.com>
2017-06-01 09:22:29 +0800

    添加：获取屏幕尺寸

---
+++ javascript/get_screen_size.js
@@ -0,0 +1,14 @@
+var winSize = function() {
+    var e = window,
+        a = 'inner';
+ 
+    if ( !('innerWidth' in window) ){
+        a = 'client';
+        e = document.documentElement || document.body;
+    }
+ 
+    return { width : e[ a + 'Width' ] , height : e[ a + 'Height' ] };
+};
+ 
+winSize().height;   // height
+winSize().width;    // width
